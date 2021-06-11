958c54067d4593b8b13ac1bd4f9328a516c0fd1d
Richard Schmidt <richardnew12511@gmail.com>
2021-06-11 15:57:09 -0500

    35

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#landing_page'
+  root "home#index"
+  get 'home/index'
   get 'static_pages/landing_page'
   get 'privacy_policy', to: "static_pages#privacy_policy"
 
